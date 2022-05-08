fd589a6f5bbd792c7b4aad341cac88f8959b6704
liken999 <dawidkorzeniewski1@wp.pl>
2022-05-08 16:44:30 +0200

    Hello World
    view added

--- amd2/urls.py
+++ amd2/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('', RedirectView.as_view(url='polls/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('polls.urls')),
+    #path('', RedirectView.as_view(url='polls/', permanent=True)),
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
