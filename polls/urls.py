e6f7e7f321e91e5e44989181a31f6a47ffc41a7d
Athamed <46008815+Athamed@users.noreply.github.com>
2022-05-08 22:33:12 +0200

    Step 5 and 6
    Steps done without and challenges/

--- polls/urls.py
+++ polls/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from . import views
 
 
+# urlpatterns = [
+#    path('', views.home, name="home"),
+# ]
+
 urlpatterns = [
-    path('', views.home, name="home"),
-
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
-
-
