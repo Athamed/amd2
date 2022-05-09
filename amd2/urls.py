7290048dded3771707fd6b0d9cf1c9a74805928a
Athamed <46008815+Athamed@users.noreply.github.com>
2022-05-09 09:03:19 +0200

    Step 8 mostly done

--- amd2/urls.py
+++ amd2/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('', RedirectView.as_view(url='polls/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
