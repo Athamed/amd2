6e546c777d6bc1286f6267b656e30c5940a6a9b0
Athamed <46008815+Athamed@users.noreply.github.com>
2022-05-21 13:39:48 +0200

    Added form to create, modify and delete authors and books
    http://127.0.0.1:8000/polls/author/create/ - to create author
    http://127.0.0.1:8000/polls/author/ID/update/ - to update author - change ID to number
    http://127.0.0.1:8000/polls/author/ID/delete/ - to delete author
    
    http://127.0.0.1:8000/polls/book/create/ - to create book
    http://127.0.0.1:8000/polls/book/ID/update/ - to update book - change ID to number
    http://127.0.0.1:8000/polls/book/ID/delete/ - to delete book

--- polls/models.py
+++ polls/models.py
@@ -29,17 +29,13 @@ class Language(models.Model):
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
     title = models.CharField(max_length=200)
-
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True,
-                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
 
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
@@ -98,12 +94,6 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
-
-    @property
-    def is_overdue(self):
-        if self.due_back and date.today() > self.due_back:
-            return True
-        return False
 
 
 class Author(models.Model):
